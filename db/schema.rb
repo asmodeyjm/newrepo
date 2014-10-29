d0397beae14d6f210374b506c775ddfb3c5c37bb
Marco Jovovich <mtjovovich@gmail.com>
2014-10-29 19:55:21 -0400

    voting

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20141028003743) do
 
+  
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -29,6 +32,7 @@ ActiveRecord::Schema.define(version: 20141028003743) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
     t.float    "rank"
   end
 
@@ -67,5 +71,16 @@ ActiveRecord::Schema.define(version: 20141028003743) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "votes", force: true do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
