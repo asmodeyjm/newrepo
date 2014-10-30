3023bbe0d6d37527ebb9fae8281cf58a5153a98e
Marco Jovovich <mtjovovich@gmail.com>
2014-10-29 20:24:27 -0400

    more rspec tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,12 +8,11 @@
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
-  
-  mount_uploader :image, ImageUploader
+  validates :topic, :presence: true
+  validates :user :presence :true
 
-  after_create :create_vote
+
+  mount_uploader :image, ImageUploader
 
   def up_votes
     votes.where(value: 1).count
@@ -34,7 +33,7 @@
     update_attribute(:rank, new_rank)
   end
 
-  private
+  
 
   def create_vote
     user.votes.create(value: 1, post: self)
